7efdf8872d8239c81454d33ff1938b7bf759f6cb
jumplee <liyuntao09@gmail.com>
2015-04-30 17:05:49 +0800

    大的功能提升
    加入include标签，测试自动化成功，字符串变量实现

--- README.md
+++ README.md
@@ -13,17 +13,100 @@ mac or linux user
 ```dash
 sudo npm install
 ```
-4. as normal user in china you may have to change you npm source to **taobao** use **nrm**
+4. 天朝屌丝记得把 npm源改为 **taobao** use **nrm**
   [nrm](http://www.tuicool.com/articles/nYjqeu)
 5. run as 
 ```dash
   gulp compile_template
 ```
-:-D
+
 
 6. Gulp will generate the tpl.js ,add it to the sencha app.json,or any way,make it loaded in the index.html.
+7. you can add watch for the html files to auto compile
+```dash
+gulp watch
+//just enjoy it
+//:-D
+```
 
-#notices
-1. I add a watch to make it auto compile ,it works fine in PC,but fails in Mac.
-  just watch the code and find how to use it.
-2. As the files grow ,you may find it will slow down ,because I concat all the files to the tpl.js.You can just **add another folder and gulp task** to compile a **tpl2.js**
+#usage
+
+1.You can use with a variable.
+
+
+```javascript
+
+{
+    xtype:'panel',
+    tpl:_.tpl.name.Info
+}
+```
+2. I recommend you to use this way for DI.
+
+    1.override component class
+```javascript
+//修改Ext的模板功能，可以通过变量配置html来实现
+Ext.Component.override({
+applyTpl: function(config) {
+//看是不是类似 name.info 这样的单词
+if(typeof config=='string'&&/^\w*(\.\w*)*$/.test(config)){
+//var tpl_ar=tpl.split('.');
+//for(var i=0;i<tpl_ar.length;i++){
+//	_.tpl[tpl[i]]
+//}
+//获取变量
+eval('var realTpl=_.tpl.'+config);
+//如果数组中有这个变量就说明 tpl解析正确
+if(realTpl){
+//用真实的模板覆盖config
+config=realTpl;
+}
+}
+//其他情况说明，这个配置是普通的tpl就直接走老的逻辑
+return (Ext.isObject(config) && config.isTemplate) ? config : new Ext.XTemplate(config);
+}
+});
+//修改DataView的itemTpl功能，可以通过变量配置html来实现
+Ext.DataView.override({
+applyItemTpl: function(config) {
+//看是不是类似 name.info 这样的单词
+if(typeof config=='string'&&/^\w*(\.\w*)*$/.test(config)){
+//var tpl_ar=tpl.split('.');
+//for(var i=0;i<tpl_ar.length;i++){
+//	_.tpl[tpl[i]]
+//}
+//获取变量
+eval('var realTpl=_.tpl.'+config);
+//如果数组中有这个变量就说明 tpl解析正确
+if(realTpl){
+//用真实的模板覆盖config
+config=realTpl;
+}
+}
+//其他情况说明，这个配置是普通的tpl就直接走老的逻辑
+return (Ext.isObject(config) && config.isTemplate) ? config : new Ext.XTemplate(config);
+}
+})
+
+```
+I'm chinese ,so enjoy the beauty of Chinese. ⊙﹏⊙b汗
+
+2. use like that:
+
+```javascript
+{
+xtype:'panel',
+tpl:'name.info'
+}
+
+```
+3. with [gulp-html-include](https://www.npmjs.com/package/gulp-html-tag-include) you  can use **<inlcude> tag** to save your life .
+
+```html
+<div class="info">
+<include src="user.html"></inlcude>
+</div>
+```
+
+#notices&tips
+1. As the files grow ,you may find it will slow down ,because I concat all the files to the tpl.js.You can just **add another folder and gulp task** to compile a **tpl2.js**
--- gulp.js
+++ gulp.js
@@ -6,39 +6,47 @@ var wrap = require("gulp-wrap");
 var declare = require('gulp-declare');
 var watch = require('gulp-watch');
 var connect = require('gulp-connect');
-
-
+var include = require('gulp-html-tag-include');
+//执行代码
+var exec = require('child_process').exec;
 gulp.task('default', ['compile_template']);
-
+gulp.task('tpl',['compile_template']);
 
 
 var paths={
 	 //不能以"./"开头否则gulp-watch会不能正常检查出文件的增加和减少
 	tpl:['resources/tpl/*/*','resources/tpl/*']
 }
+
 /*
  var cmd = require('run-cmd');
-var sassCmd = cmd.build('compass')
-  .flag('compile', 'compile') // creates a function all(), which will generate -a when called.
-  .flag('watch', 'watch') // crates a function list(), which will generate -l when called.
-  .default('watch');
-*/
-
+ var sassCmd = cmd.build('compass')
+ .flag('compile', 'compile') // creates a function all(), which will generate -a when called.
+ .flag('watch', 'watch') // crates a function list(), which will generate -l when called.
+ .default('watch');
+ */
 gulp.task('compile_template', function(done) {
   gulp.src(paths.tpl)
-    .pipe(replace('\'','\\\''))
-    .pipe(replace(/(\n|\r|(\r\n)|(\u0085)|(\u2028)|(\u2029))/g,''))
-    .pipe(wrap("'<%= contents %>'"))
-    .pipe(declare({
-      namespace: 'MPDL.tpl',
-      noRedeclare: true // 避免变量名重复
-    }))
-   .pipe(concat('tpl.js'))
-    .pipe(gulp.dest('./resources/js'))
-    .on('end', done);
+      //加入include标签
+      .pipe(include())
+      //去除单引号和空格
+      .pipe(replace('\'','\\\''))
+      .pipe(replace(/(\n|\r|(\r\n)|(\u0085)|(\u2028)|(\u2029))|/g,''))
+      //.pipe(replace(/(^>\s*)|(\s*<$)/g,''))
+      //拼接成一个大的字符串
+      .pipe(wrap("'<%= contents %>'"))
+      //声明变量
+      .pipe(declare({
+        namespace: 'MPDL.tpl',
+        noRedeclare: true // 避免变量名重复
+      }))
+      //连接成一个文件
+      .pipe(concat('tpl.js'))
+      .pipe(gulp.dest('./resources/js'))
+      .on('end', done);
 });
-
-gulp.task('watch', function() {
+//livereload 不实用，直接在chrome中开发更好使
+gulp.task('watch_bak', function() {
     watch(paths.tpl,function(){
         gulp.start('compile_template');
     });
@@ -46,18 +54,42 @@ gulp.task('watch', function() {
     server = livereload.createServer();
     server.watch(__dirname +'/app');
 });
-	
-gulp.task('sass', function() {
-	sassCmd.compile('./resources/sass/theme.scss');
+//检测tpl自动更新
+gulp.task('watch', function() {
+    watch(paths.tpl,function(){
+        gulp.start('compile_template');
+    });
 });
-gulp.task('watch1', function() {
+
+//没有测试通过，发现自带compass watch挺方便的不用了
+var compass = require('gulp-compass');
+gulp.task('compass', function() {
+    gulp.src('./resources/theme/theme2.scss')
+        .pipe(compass({
+            css: './resources/theme/stylesheets',
+            sass: './resources/theme/theme2.scss'
+        }))
+        .pipe(gulp.dest('./resources/css'));
+});
+gulp.task('sass', function() {
+	sassCmd.compile();
+});
+gulp.task('watch_tpl', function() {
     watch(paths.tpl,function(){
         gulp.start('compile_template');
     });
 });
 gulp.task('server', function(next) {
     connect.server({
-        root: '.'
+        root: './shareWeb',
+        livereload: true
     });
 });
+//jsDoc 不支持sencha 使用jsDuck，放弃
+//var jsdoc = require("gulp-jsdoc");
+//gulp.task('doc', function(next) {
+//    gulp.src("./D/fun.js")
+//        .pipe(jsdoc('./document'))
+//});
+
 
